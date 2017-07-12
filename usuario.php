c1b4ed709560240638f27c0fe1afdf73d07b72c3
MatiDiaz <matias@elmundoalreves.cl>
2017-07-11 23:14:03 -0400

    guardar usuario

--- usuario.php
+++ usuario.php
@@ -13,5 +13,48 @@ and open the template in the editor.
         <?php
         // put your code here
         ?>
+        <div>
+            <div>
+                agregar
+                <div>
+                    Agregar Cliente
+                    <div>
+                        <form>
+                            <table>
+                                <tr>
+                                    <td>Rut:</td>
+                                    <td><input type="text" name="txtRut"></td>
+                                </tr>
+                                <tr>
+                                    <td>Password:</td>
+                                    <td><input type="password" name="txtPassword"></td>
+                                </tr>
+                                <tr>
+                                    <td>Nombre Completo:</td>
+                                    <td><input type="text" name="txtNombre"></td>
+                                </tr>
+                                <tr>
+                                <select name="cboPelicula">
+                                        <option value="" selected="">Elige</option>
+                                        <?php
+                                        $var = new DaoUsuario();
+                                        $var = $var->mostrarCartelera();
+                                        while ($rows = mysqli_fetch_array($var)) {
+                                            ?>
+                                            <option value="<?php echo $rows['codigo'] ?>"> <?php echo $rows['titulo']; ?></option>   
+                                            <?php
+                                        }
+                                        ?>
+                                    </select> 
+                                </tr>
+                                <tr>
+                                    <td colspan="2"><input type="submit" name="btnGuardar"></td>
+                                </tr>
+                            </table>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
